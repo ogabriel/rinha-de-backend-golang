fa32679d61e1c5b02153856dcf0fd02cbadcb78f
ogabriel <gabrieloliver8991@gmail.com>
2023-11-03 15:01:10 -0300

    feat: add Pessoa struct

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/ogabriel/rinha-de-backend-golang
 
 go 1.21.1
 
-require github.com/gin-gonic/gin v1.9.1
+require (
+	github.com/gin-gonic/gin v1.9.1
+	github.com/google/uuid v1.4.0
+)
 
 require (
 	github.com/bytedance/sonic v1.9.1 // indirect
--- main.go
+++ main.go
@@ -2,7 +2,16 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
+
+type Pessoa struct {
+	ID         uuid.UUID `json:"id"`
+	Apelido    string    `json:"apelido"`
+	Nome       string    `json:"nome"`
+	Nascimento string    `json:"nascimento"`
+	Stack      []string  `json:"stack"`
+}
 
 func main() {
 	router := gin.Default()
