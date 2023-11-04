26639a93416e75f2fe37df40b821f3fa180cf350
ogabriel <gabrieloliver8991@gmail.com>
2023-11-04 15:36:26 -0300

    fix: docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,8 @@
 if [ "$1" = 'release' ]; then
     exec /app/rinha
 elif [ "$1" = 'migrate_and_release' ]; then
-    psql $DATABASE_URL -c "CREATE DATABASE $DATABASE_NAME"
     psql $DATABASE_URL -c "DROP DATABASE $DATABASE_NAME"
+    psql $DATABASE_URL -c "CREATE DATABASE $DATABASE_NAME"
     /app/migrate -path /app/migrations/ -database $DATABASE_URL/$DATABASE_NAME?sslmode=disable -verbose up
 
     exec /app/rinha
