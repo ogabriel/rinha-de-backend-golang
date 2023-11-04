d25a753347e280b3b71aa79d2b2f6d2d3137f778
ogabriel <gabrieloliver8991@gmail.com>
2023-11-04 19:19:34 -0300

    refactor: use handler functions to gin routing

--- main.go
+++ main.go
@@ -23,8 +23,6 @@ type Pessoa struct {
 	Stack      []string  `json:"stack"`
 }
 
-var pool *pgxpool.Pool
-
 func main() {
 	connString := "postgres://postgres:postgres@127.0.0.1:5432/rinha?sslmode=disable&pool_min_conns=1&pool_max_conns=15"
 
@@ -34,7 +32,7 @@ func main() {
 		panic(err)
 	}
 
-	pool, err = pgxpool.NewWithConfig(context.Background(), config)
+	pool, err := pgxpool.NewWithConfig(context.Background(), config)
 
 	defer pool.Close()
 
@@ -46,10 +44,10 @@ func main() {
 	router := gin.New()
 	router.Use(gin.Recovery())
 
-	router.POST("/pessoas", postPessoas)
-	router.GET("/pessoas/:id", getPessoas)
-	router.GET("/pessoas", indexPessoas)
-	router.GET("/contagem-pessoas", contagemPessoas)
+	router.POST("/pessoas", postPessoas(pool))
+	router.GET("/pessoas/:id", getPessoas(pool))
+	router.GET("/pessoas", indexPessoas(pool))
+	router.GET("/contagem-pessoas", contagemPessoas(pool))
 
 	port, ok := os.LookupEnv("PORT")
 
@@ -60,49 +58,51 @@ func main() {
 	router.Run("localhost:" + port)
 }
 
-func postPessoas(c *gin.Context) {
-	var person Pessoa
+func postPessoas(pool *pgxpool.Pool) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var person Pessoa
 
-	if err := c.BindJSON(&person); err != nil {
-		c.Status(http.StatusBadRequest)
-		return
+		if err := c.BindJSON(&person); err != nil {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+
+		if invalidStack(person.Stack) {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+
+		if _, err := time.Parse("2006-01-02", person.Nascimento); err != nil {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+
+		uuid, err := uuid.NewRandom()
+
+		if err != nil {
+			c.Status(http.StatusUnprocessableEntity)
+			return
+		}
+
+		_, err = pool.Exec(
+			context.Background(),
+			"INSERT INTO pessoas (id, apelido, nome, nascimento, stack, busca) VALUES ($1, $2, $3, $4, $5, $6)",
+			uuid,
+			person.Apelido,
+			person.Nome,
+			person.Nascimento,
+			person.Stack,
+			buildBusca(person),
+		)
+
+		if err != nil {
+			c.Status(http.StatusUnprocessableEntity)
+			return
+		}
+
+		c.Header("Location", "/pessoas/"+uuid.String())
+		c.Status(http.StatusCreated)
 	}
-
-	if invalidStack(person.Stack) {
-		c.Status(http.StatusBadRequest)
-		return
-	}
-
-	if _, err := time.Parse("2006-01-02", person.Nascimento); err != nil {
-		c.Status(http.StatusBadRequest)
-		return
-	}
-
-	uuid, err := uuid.NewRandom()
-
-	if err != nil {
-		c.Status(http.StatusUnprocessableEntity)
-		return
-	}
-
-	_, err = pool.Exec(
-		context.Background(),
-		"INSERT INTO pessoas (id, apelido, nome, nascimento, stack, busca) VALUES ($1, $2, $3, $4, $5, $6)",
-		uuid,
-		person.Apelido,
-		person.Nome,
-		person.Nascimento,
-		person.Stack,
-		buildBusca(person),
-	)
-
-	if err != nil {
-		c.Status(http.StatusUnprocessableEntity)
-		return
-	}
-
-	c.Header("Location", "/pessoas/"+uuid.String())
-	c.Status(http.StatusCreated)
 }
 
 func invalidStack(stack []string) bool {
@@ -133,44 +133,51 @@ func buildBusca(person Pessoa) string {
 	return busca.String()
 }
 
-func getPessoas(c *gin.Context) {
-	id := c.Param("id")
+func getPessoas(pool *pgxpool.Pool) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := c.Param("id")
 
-	var person Pessoa
+		var person Pessoa
 
-	if err := pool.QueryRow(context.Background(), "SELECT id, apelido, nome, nascimento, stack FROM pessoas WHERE id = $1", id).Scan(&person.ID, &person.Apelido, &person.Nome, &person.Nascimento, &person.Stack); err != nil {
-		c.Status(http.StatusNotFound)
-		return
+		if err := pool.QueryRow(context.Background(), "SELECT id, apelido, nome, nascimento, stack FROM pessoas WHERE id = $1", id).Scan(&person.ID, &person.Apelido, &person.Nome, &person.Nascimento, &person.Stack); err != nil {
+			c.Status(http.StatusNotFound)
+			return
+		}
+
+		c.JSON(http.StatusOK, person)
+
 	}
-
-	c.JSON(http.StatusOK, person)
 }
 
-func indexPessoas(c *gin.Context) {
-	term, passed := c.GetQuery("t")
+func indexPessoas(pool *pgxpool.Pool) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		term, passed := c.GetQuery("t")
 
-	if !passed || term == "" {
-		c.Status(http.StatusBadRequest)
-		return
+		if !passed || term == "" {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+
+		term = strings.ToLower(term)
+
+		rows, err := pool.Query(context.Background(), "SELECT id, apelido, nome, nascimento, stack FROM pessoas WHERE busca LIKE '%' || $1 || '%' LIMIT 50", term)
+
+		if err != nil {
+			c.Status(http.StatusUnprocessableEntity)
+			return
+		}
+
+		people, err := pgx.CollectRows(rows, pgx.RowToStructByPos[Pessoa])
+
+		c.JSON(http.StatusOK, people)
 	}
-
-	term = strings.ToLower(term)
-
-	rows, err := pool.Query(context.Background(), "SELECT id, apelido, nome, nascimento, stack FROM pessoas WHERE busca LIKE '%' || $1 || '%' LIMIT 50", term)
-
-	if err != nil {
-		c.Status(http.StatusUnprocessableEntity)
-		return
-	}
-
-	people, err := pgx.CollectRows(rows, pgx.RowToStructByPos[Pessoa])
-
-	c.JSON(http.StatusOK, people)
 }
 
-func contagemPessoas(c *gin.Context) {
-	var count int
-	_ = pool.QueryRow(context.Background(), "SELECT COUNT(*) FROM pessoas").Scan(&count)
+func contagemPessoas(pool *pgxpool.Pool) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var count int
+		_ = pool.QueryRow(context.Background(), "SELECT COUNT(*) FROM pessoas").Scan(&count)
 
-	c.String(http.StatusOK, strconv.Itoa(count))
+		c.String(http.StatusOK, strconv.Itoa(count))
+	}
 }
