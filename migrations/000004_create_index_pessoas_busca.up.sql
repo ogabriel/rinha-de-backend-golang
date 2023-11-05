2fe9c4638dff01faf3317eaa24870cae6a2b9cbe
ogabriel <gabrieloliver8991@gmail.com>
2023-11-05 16:54:00 -0300

    fix: raise siglen of busca index

--- migrations/000004_create_index_pessoas_busca.up.sql
+++ migrations/000004_create_index_pessoas_busca.up.sql
@@ -1,1 +1,1 @@
-CREATE INDEX pessoas_busca_gist_trgm_ops_index ON pessoas USING gist (busca gist_trgm_ops (siglen = '256'));
+CREATE INDEX pessoas_busca_gist_trgm_ops_index ON pessoas USING gist (busca gist_trgm_ops (siglen = '1024'));
