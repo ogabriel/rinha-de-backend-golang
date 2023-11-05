8095437c812c86fae462f969cadb42a3f99f4370
ogabriel <gabrieloliver8991@gmail.com>
2023-11-05 11:20:32 -0300

    refactor: update docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
 if [ "$1" = 'release' ]; then
-    exec /app/rinha
+    exec /app/rinha-de-backend-golang
 elif [ "$1" = 'migrate_and_release' ]; then
-    psql $DATABASE_URL -c "DROP DATABASE $DATABASE_NAME"
-    psql $DATABASE_URL -c "CREATE DATABASE $DATABASE_NAME"
-    /app/migrate -path /app/migrations/ -database $DATABASE_URL/$DATABASE_NAME?sslmode=disable -verbose up
-
-    exec /app/rinha
+    make database-reset
+    exec /app/rinha-de-backend-golang
 fi
